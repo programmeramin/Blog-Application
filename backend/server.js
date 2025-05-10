8a079fd4db59b5027589b0cf126ebf9641f23bc4
Programmeramin <developeramin2615@gmail.com>
2025-05-10 20:15:20 +0600

    contact page and contact api create success

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import authRoute from './routes/authRoute.js';
 import postRoute from './routes/postRoute.js';
 import userRoutes from './routes/userRoute.js'
 import commentRoute from './routes/commentRoutes.js';
+import contactRoute from "./routes/contactRoute.js"
 
 // dotenv config
 dotenv.config();
@@ -33,7 +34,8 @@ app.use('/api/auth', authRoute);
 app.use('/api/users', userRoutes);
 app.use('/api/blog', postRoute);
 app.use("/api/comments", commentRoute);
-
+app.use("/api", contactRoute);
+  
 // lisetening server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
